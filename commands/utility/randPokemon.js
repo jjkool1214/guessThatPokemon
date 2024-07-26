8ab8e23082638499e4e864945c3a831c0c75fa90
jjkool1214 <jaysenstolarz@gmail.com>
2024-07-25 23:27:44 -0400

    Got it functioning

--- commands/utility/randPokemon.js
+++ commands/utility/randPokemon.js
@@ -4,19 +4,22 @@ var option
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("randomPokemon")
+		.setName("pokemon")
 		.setDescription('generates a random pokemon'),
 
 	async execute(interaction) {
         const num = Math.floor(Math.random() * 1025) + 1
-        const URL = "https://pokeapi.co/api/v2/pokemon/" + num
-        const clear = true
-        const pokemon = null
+        const URL = "https://pokeapi.co/api/v2/pokemon/".concat(num).concat("/")
+        let finding = await fetch(URL);
+        let pokemon = await finding.json()
+        console.log(pokemon)
+        console.log(pokemon["types"].length)
 
-        fetch(URL).then(response => {
-            pokemon = response.json();
-        })
+        if(pokemon["types"].length < 2){
+            await interaction.reply("Name : " + pokemon["name"] + " \nType : " + pokemon["types"][0]["type"]["name"] + "\nPhoto : " + pokemon["sprites"]["front_default"])
+        } else {
+            await interaction.reply("Name : " + pokemon["name"] + " \nType : " + pokemon["types"][0]["type"]["name"] + " & " + pokemon["types"][1]["type"]["name"] + "\nPhoto : " + pokemon["sprites"]["front_default"])
+        }
 
-        interaction.reply("Name : " + pokemon["name"] + " \n Photo : " + pokemon["sprites"]["front_default"])
 	},
 };
